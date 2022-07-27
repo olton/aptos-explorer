03da651eb22f558ae73e729bf3080ede7784b815
Serhii Pimenov <serhii@pimenov.com.ua>
2022-07-27 08:15:09 +0300

    update db

--- src/db/indexes.sql
+++ src/db/indexes.sql
@@ -4,3 +4,4 @@ CREATE INDEX idx_btree_transactions_type ON transactions USING BTREE ((payload->
 create index idx_transactions_success on transactions (success);
 create index idx_transactions_type on transactions (type);
 create index idx_block_metadata_transactions_timestamp on block_metadata_transactions (timestamp);
+create index idx_user_transactions_timestamp on user_transactions (timestamp desc);
