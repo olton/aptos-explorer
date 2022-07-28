9213db21c7d88bcc5ce607a81ad118fc1bebe3b5
Serhii Pimenov <serhii@pimenov.com.ua>
2022-07-28 19:25:10 +0300

    update db

--- src/db/routines.sql
+++ src/db/routines.sql
@@ -26,3 +26,39 @@ BEGIN
 	END IF;
 END
 $$;
+
+create function trans_type_count_reset_trigger() returns trigger
+    language plpgsql
+as
+$$
+DECLARE
+	tablename text;
+BEGIN
+	tablename := 'transaction_type_count';
+
+	EXECUTE 'update ' || tablename || ' set counter = 0';
+END
+$$;
+
+alter function trans_type_count_reset_trigger() owner to indexer;
+
+create function trans_type_count_update_trigger() returns trigger
+    language plpgsql
+as
+$$
+DECLARE
+	tablename text;
+BEGIN
+	tablename := 'transaction_type_count';
+
+	IF TG_OP = 'INSERT' THEN
+	    EXECUTE 'update ' || tablename || ' set counter = counter + 1 where type = $1'
+        USING NEW.type;
+
+		RETURN NEW;
+	END IF;
+END
+$$;
+
+alter function trans_type_count_update_trigger() owner to indexer;
+
--- src/db/triggers.sql
+++ src/db/triggers.sql
@@ -5,3 +5,18 @@ CREATE CONSTRAINT TRIGGER trans_count_update_trigger
 CREATE TRIGGER rows_count_reset_trigger
 	AFTER TRUNCATE ON transactions
 	FOR EACH STATEMENT EXECUTE PROCEDURE trans_type_count_reset_trigger();
+
+-- auto-generated definition
+create constraint trigger trans_status_count_update_trigger
+    after insert
+    on transactions
+    deferrable initially deferred
+    for each row
+execute procedure trans_status_count_update_trigger();
+
+-- auto-generated definition
+create trigger rows_status_count_reset_trigger
+    after truncate
+    on transactions
+execute procedure trans_status_count_reset_trigger();
+
